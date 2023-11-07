f8800d2c4779805798981a51a40c7ab8ea276c2c
Qubackii <kuba.29000@gmail.com>
2023-11-07 19:34:35 +0100

    database connection added, client-server and threads

--- src/main/java/Database/DatabaseConnection.java
+++ src/main/java/Database/DatabaseConnection.java
@@ -1,2 +1,19 @@
-package Database;public class DatabaseConnection {
+package Database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/healthguardian";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Błąd podczas nawiązywania połączenia z bazą danych.");
+        }
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.healthguardian.healthguardian {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
-    opens com.healthguardian.healthguardian to javafx.fxml;
-    exports com.healthguardian.healthguardian;
+    opens com.healthguardian to javafx.fxml;
+    exports com.healthguardian;
 }
