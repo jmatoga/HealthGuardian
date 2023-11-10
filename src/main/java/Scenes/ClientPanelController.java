f4c30ab6cde7a461e7334363b8a68904a71a999e
Kraisu0 <krzysztofpopiela@poczta.fm>
2023-11-10 21:10:41 +0100

    Zaczęte user panel

--- src/main/java/Scenes/ClientPanelController.java
+++ src/main/java/Scenes/ClientPanelController.java
@@ -1,42 +1,53 @@
 package Scenes;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.awt.*;
 import java.io.IOException;
+import java.net.URL;
 import java.sql.Date;
 import java.sql.SQLOutput;
 import java.text.BreakIterator;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
 
 public class ClientPanelController {
 
-    private volatile boolean stop = false;
+    //private volatile boolean stop = false;
 
     @FXML
-    private Label timeLabel;
+    private Label dateLabel;
 
-//    private void Timenow() {
-//        Thread thread = new Thread( () ->{
-//            SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
-//            while (!stop) {
-//                try {
-//                    Thread.sleep(1000);
-//                } catch (Exception e) {
-//                    System.out.println("Data - Errror");
-//                }
-//                final String timenow = sdf.format(new SimpleDateFormat());
-//                Platform.runLater(() -> {
-//
-//                    timeLabel.setText(timenow);
-//                });
-//            }
-//        });
-//        thread.start();
-//    }
+
+    public void initialize(URL location, ResourceBundle resources) {
+        // Utwórz Timeline do cyklicznego odświeżania daty co sekundę
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> updateDateTime()));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+    private void updateDateTime() {
+        // Pobierz aktualną datę i godzinę
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        // Utwórz formatter daty i godziny
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        // Sformatuj datę i godzinę jako tekst
+        String formattedDateTime = currentDateTime.format(formatter);
+
+        // Ustaw sformatowaną datę i godzinę jako tekst etykiety
+        dateLabel.setText(formattedDateTime);
+        System.out.println(formattedDateTime);
+    }
 
 }
