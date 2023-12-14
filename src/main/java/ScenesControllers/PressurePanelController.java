b33f9c80f5496de08ce81e5dd84ddd116f786f7e
jmatoga <jakub.matoga.it@gmail.com>
2023-12-14 23:18:47 +0100

    Added style of chart
    Signed-off-by: jmatoga <jakub.matoga.it@gmail.com>

--- src/main/java/ScenesControllers/PressurePanelController.java
+++ src/main/java/ScenesControllers/PressurePanelController.java
@@ -4,6 +4,8 @@ import Client.Client;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import utils.Message;
 
@@ -22,6 +24,9 @@ public class PressurePanelController implements Initializable {
     private Button userPanelButton;
 
     @FXML
+    private LineChart<?, ?> pressureChart;
+
+    @FXML
     private void userPanelButtonClicked(ActionEvent event) throws IOException, IOException {
         new SceneSwitch("/ScenesLayout/ClientPanelScene.fxml");
     }
@@ -30,5 +35,19 @@ public class PressurePanelController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         PressurePanelController.ReadFromServer = Client.ReadFromServer;
         PressurePanelController.SendToServer = Client.SendToServer;
+
+        pressureChart.getXAxis().setLabel("Timeline");
+        pressureChart.getYAxis().setLabel("Pressure");
+
+        XYChart.Series series = new XYChart.Series();
+        series.getData().add(new XYChart.Data<>("1", 23));
+        series.getData().add(new XYChart.Data<>("2", 123));
+        series.getData().add(new XYChart.Data<>("3", 53));
+
+        XYChart.Series series1 = new XYChart.Series();
+        series1.getData().add(new XYChart.Data<>("1", 5));
+        series1.getData().add(new XYChart.Data<>("5", 23));
+        series1.getData().add(new XYChart.Data<>("8", 70));
+        pressureChart.getData().addAll(series, series1);
     }
 }
